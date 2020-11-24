10568b5dc92c056713bee2a1af1694d08b0f58d8
Monica Jalem <monica.jalem1@gmail.com>
2020-11-24 21:37:09 +0000

    First visual studio project with Github. First commit to see how it works!

---
+++ FirstGitVisualStudio/FirstGitVisualStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstGitVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Visual Studio project with GitHub");
+        }
+    }
+}
